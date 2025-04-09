e9929ecb971a7308ac5f8b908ce86754c296d1b1
justin <kinhanjustin@gmail.com>
2025-04-09 16:00:29 +0200

    Training toegevoegd

---
+++ model.js
@@ -0,0 +1,80 @@
+ml5.setBackend("webgl");
+const options = {
+    task: 'classification',
+    debug: true,
+    layers: [
+        { type: 'dense', units: 32, activation: 'relu' },
+        { type: 'dense', units: 32, activation: 'relu' },
+        { type: 'dense', units: 32, activation: 'relu' },
+        { type: 'dense', activation: 'softmax' },
+    ],
+    learningRate: 0.01,
+    epochs: 10
+};
+const nn = ml5.neuralNetwork(options);
+
+const trainButton = document.getElementById("trainButton");
+const testButton = document.getElementById("testButton");
+
+trainButton.addEventListener("click", fetchTrainingData);
+testButton.addEventListener("click", testModel);
+
+let testData = [];
+
+async function fetchTrainingData() {
+    fetch("TrainingData.json")
+        .then((response) => response.json())
+        .then((data) => {
+            const flatData = [];
+            for (let label in data) {
+                data[label].forEach(poseData => {
+                    flatData.push({pose: poseData, label: label});
+                });
+            }
+            flatData.sort(() => Math.random() - 0.5);
+
+            const splitIndex = Math.floor(flatData.length * 0.8);
+            const trainData = flatData.slice(0, splitIndex);
+            testData = flatData.slice(splitIndex);
+
+            trainNN(trainData);
+        });
+}
+
+async function trainNN(trainingData) {
+    trainingData.forEach(item => {
+        nn.addData(item.pose, {label: item.label});
+    });
+
+    nn.normalizeData();
+    nn.train({epochs: options.epochs}, () => finishedTraining());
+
+    function finishedTraining() {
+        console.log("Training complete!");
+        nn.save("model", () => console.log("Model was saved!"));
+    }
+}
+
+async function testModel() {
+    let correct = 0;
+
+    if (testData.length === 0) {
+        console.error("No test data found!");
+        return;
+    }
+
+    for (const testItem of testData) {
+        const prediction = await nn.classify(testItem.pose);
+        if (prediction[0].label === testItem.label) {
+            correct++;
+        }
+    }
+
+    const accuracy = ((correct / testData.length) * 100).toFixed(2);
+    console.log(`Model accuracy: ${accuracy}%`);
+
+    const accuracyDisplay = document.getElementById("accuracyDisplay");
+    if (accuracyDisplay) {
+        accuracyDisplay.textContent = `Model accuracy: ${accuracy}%`;
+    }
+}
